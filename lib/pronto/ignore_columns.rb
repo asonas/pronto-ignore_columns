4afef112d878e63dbf9d92527c84336858bcd3d9
Yuya Fujiwara <asonas@cookpad.com>
2019-06-20 19:16:53 +0900

    Initial commit

---
+++ lib/pronto/ignore_columns.rb
@@ -0,0 +1,8 @@
+require "pronto/ignore_columns/version"
+
+module Pronto
+  module IgnoreColumns
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pronto/ignore_columns/version.rb
@@ -0,0 +1,5 @@
+module Pronto
+  module IgnoreColumns
+    VERSION = "0.1.0"
+  end
+end
